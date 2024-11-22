7f76fcbda2bf7a43af5bd46c3015fbc783893a68
Marcus Li <mmli@lcs.on.ca>
2024-11-22 08:39:16 -0500

    Initial Commit

---
+++ FavoriteThing/FavoriteThingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteThingApp.swift
+//  FavoriteThing
+//
+//  Created by Marcus Li on 2024-11-22.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteThingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
